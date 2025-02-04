96e147213d617bf31222e92ebec8cfd40997496c
dannyRT21 <USSS025824@ugb.edu.sv>
2025-02-03 19:24:34 -0600

    notificaciones agregadas al boton calcular (no dan)

--- app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
@@ -19,6 +19,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
     Button btn;
     TextView lblRespuesta;
@@ -45,54 +47,68 @@ Spinner spn;
                     double num1 = txtNum1.getText().toString().isEmpty() ? 0 : Double.parseDouble(txtNum1.getText().toString());
                     double num2 = txtNum2.getText().toString().isEmpty() ? 0 : Double.parseDouble(txtNum2.getText().toString());
                     double resultado = 0.0;
+                    String msg = "";
                     spn = findViewById(R.id.spnOpciones);
-                    switch (spn.getSelectedItemPosition())
-                    {
-                        case 0 :
+                    switch (spn.getSelectedItemPosition()) {
+                        case 0:
                             resultado = num1 + num2;
+                            msg = "la suma es: " + resultado;
                             break;
 
-                        case 1 :
+                        case 1:
+                            msg = "la resta es: " + resultado;
                             resultado = num1 - num2;
                             break;
 
                         case 2:
-                            resultado = num1 *num2;
+                            msg = "la multiplicacion es: " + resultado;
+                            resultado = num1 * num2;
                             break;
 
                         case 3:
-                            resultado = num1 /num2;
+                            msg = "la divicion es: " + resultado;
+                            resultado = num1 / num2;
                             break;
 
                         case 4:
-                            resultado = Math.pow(num1,num2);
+                            msg = "el exponente de " + num2 + "y" + num1 + "es igual a =" + resultado;
+                            resultado = Math.pow(num1, num2);
                             break;
 
                         case 5:
+                            msg = "la multiplicacion es =" + resultado;
                             resultado = (num1 * num2) / 100;
                             break;
+
                         case 6: // Raíz cuadrada
                             if (num1 < 0) {
                                 lblRespuesta.setText("Error:la raíz de un número negativo no existe.");
                                 return;
                             }
                             resultado = Math.sqrt(num1);
+                            msg = "la raiz cuadrada de " + num1 + "es igual = " + resultado;
                             break;
                         case 7: // Factorial
                             if (num1 < 0 || num1 != (int) num1) {
                                 lblRespuesta.setText("Error: Factorial se calcula a enteros positivos.");
                                 return;
+
+
                             }
                             int n = (int) num1;
                             resultado = 1;
                             for (int i = 1; i <= n; i++) {
                                 resultado *= i;
+                                msg = "el numero factorial de " + num1 + "es igual a =" + resultado;
                             }
                             break;
+
                         default:
                             lblRespuesta.setText("Error: la Operación no da.");
                             return;
+
                     }
+
 
 
 
@@ -100,6 +116,7 @@ Spinner spn;
                     lblRespuesta.setText("Respuesta: " + resultado);
                 } catch (NumberFormatException e) {
                     lblRespuesta.setText("Error: solo podes ingresar numeros.");
+                //Toast.makeText(MainActivity.this, msg,Toast.LENGTH_LONG).show();
                 }
             }
         });
