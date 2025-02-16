636ce45766816710318c9bbd6bef50866466356f
dannyRT21 <USSS025824@ugb.edu.sv>
2025-02-15 23:58:28 -0600

    Se crea la rama del Senspr de luz y se prueba su funcionamiento

--- app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
@@ -1,4 +1,5 @@
 package com.ugb.miprimeraaplicacion;
+
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -20,64 +21,62 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+public class MainActivity extends AppCompatActivity {
+    TextView tempVal;
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        sensorLuz();
+    }
+    @Override
+    protected void onResume() { //estos metodos se llaman haciendo click derecho y en la opcion Generate/Override
+        iniciar();
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        detener();
+        super.onPause();
+    }
+    private void iniciar(){
+        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000); //es la cantidad de tiempo de ejecucion de este sensor
+    }
+    private void detener(){
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+    private void sensorLuz(){
+        tempVal = findViewById(R.id.lblSensorLuz);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT); //administrador de sensores
 
+        //if else para condicionar el funcionamiento del censor de luz
+        if( sensor==null ){
+            tempVal.setText("Tu dispositivo, NO tiene el senor de LUZ");
+            finish();
+        }
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                double valor = event.values[0];
+                tempVal.setText("Cantidad de Luz: "+ valor);
 
-
-    public class MainActivity extends AppCompatActivity{
-        TextView tempVal;
-        SensorManager sensorManager;
-        Sensor sensor;
-        SensorEventListener sensorEventListener;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            sensorLuz();
-        }
-        @Override
-        protected void onResume() {
-            iniciar();
-            super.onResume();
-        }
-        @Override
-        protected void onPause() {
-            detener();
-            super.onPause();
-        }
-        private void iniciar(){
-            sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
-        }
-        private void detener(){
-            sensorManager.unregisterListener(sensorEventListener);
-        }
-        private void sensorLuz(){
-            tempVal = findViewById(R.id.lblSensorProximidad);
-            sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-            if( sensor==null ){
-                tempVal.setText("Tu dispositivo, NO tiene el senor de PROXIMIDAD");
-                finish();
+                if(valor<=20){
+                    getWindow().getDecorView().setBackgroundColor(Color.GRAY);
+                }else if(valor<=50){
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                }else{
+                    getWindow().getDecorView().setBackgroundColor(Color.MAGENTA);
+                }
             }
-            sensorEventListener = new SensorEventListener() {
-                @Override
-                public void onSensorChanged(SensorEvent event) {
-                    double valor = event.values[0];
-                    tempVal.setText("Proximidad: "+ valor);
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-                    if(valor<=4){
-                        getWindow().getDecorView().setBackgroundColor(Color.BLACK);
-                    }else if(valor<=8){
-                        getWindow().getDecorView().setBackgroundColor(Color.MAGENTA);
-                    }else{
-                        getWindow().getDecorView().setBackgroundColor(Color.MAGENTA);
-                    }
-                }
-                @Override
-                public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-                }
-            };
-        }
-        }
+            }
+        };
+    }
+}
