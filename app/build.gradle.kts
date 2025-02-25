d8afa946abd52b42a5f382f4d68b5a346ab1f5a4
dannyRT21 <USSS025824@ugb.edu.sv>
2025-02-25 16:21:15 -0600

    se modifico la interfaz

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("androidx.cardview:cardview:1.0.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -40,4 +40,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
+
+
+
+
+
