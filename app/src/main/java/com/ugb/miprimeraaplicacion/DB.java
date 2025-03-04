cc5563a076004e7f00a079e77f9d0b87914f3b97
dannyRT21 <USSS025824@ugb.edu.sv>
2025-03-04 10:28:55 -0600

    se creo no se que pero fue en la clase donde no dio el emulador

--- app/src/main/java/com/ugb/miprimeraaplicacion/DB.java
+++ app/src/main/java/com/ugb/miprimeraaplicacion/DB.java
@@ -1,6 +1,7 @@
 package com.ugb.miprimeraaplicacion;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -28,53 +29,61 @@ public class DB extends SQLiteOpenHelper {
      * @param db Instancia de la base de datos.
      */
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        // Ejecuta la sentencia SQL para crear la tabla amigos
-        db.execSQL(SQLdb);
-    }
+        public void onCreate(SQLiteDatabase db) {
+            // Ejecuta la sentencia SQL para crear la tabla amigos
+            db.execSQL(SQLdb);
+        }
 
-    /**
-     * Método llamado cuando la base de datos necesita ser actualizada.
-     * @param db Instancia de la base de datos.
-     * @param oldVersion Versión anterior de la base de datos.
-     * @param newVersion Nueva versión de la base de datos.
-     */
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // Actualizar la estructura de la base de datos si es necesario
-    }
+        /**
+         * Método llamado cuando la base de datos necesita ser actualizada.
+         * @param db Instancia de la base de datos.
+         * @param oldVersion Versión anterior de la base de datos.
+         * @param newVersion Nueva versión de la base de datos.
+         */
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // Actualizar la estructura de la base de datos si es necesario
+        }
 
-    /**
-     * Método para administrar amigos en la base de datos.
-     * @param accion Acción a realizar (agregar, modificar, eliminar).
-     * @param datos Datos del amigo.
-     * @return Mensaje de resultado de la operación.
-     */
-    public String administrar_amigos(String accion, String[] datos) {
-        try {
-            // Obtiene una instancia de la base de datos en modo escritura
-            SQLiteDatabase db = getWritableDatabase();
-            String mensaje = "ok", sql = "";
-            // Determina la acción a realizar y construye la sentencia SQL correspondiente
-            switch (accion) {
-                case "agregar":
-                    sql = "INSERT INTO amigos (nombre, direccion, telefono, email, dui, urlFoto) VALUES ('" + datos[1] + "', '" + datos[2] + "', '" + datos[3] + "', '" + datos[4] + "', '" + datos[5] + "', '" + datos[6] + "')";
-                    break;
-                case "modificar":
-                    sql = "UPDATE amigos SET nombre = '" + datos[1] + "', direccion = '" + datos[2] + "', telefono = '" + datos[3] + "', email = '" + datos[4] + "', dui = '" + datos[5] + "', urlFoto = '" + datos[6] + "' WHERE idAmigo = " + datos[0];
-                    break;
-                case "eliminar":
-                    sql = "DELETE FROM amigos WHERE idAmigo = " + datos[0];
-                    break;
+        /**
+         * Método para administrar amigos en la base de datos.
+         * @param accion Acción a realizar (agregar, modificar, eliminar).
+         * @param datos Datos del amigo.
+         * @return Mensaje de resultado de la operación.
+         */
+        public String administrar_amigos(String accion, String[] datos) {
+            try {
+                // Obtiene una instancia de la base de datos en modo escritura
+                SQLiteDatabase db = getWritableDatabase();
+                String mensaje = "ok", sql = "" ;
+                // Determina la acción a realizar y construye la sentencia SQL correspondiente
+                switch (accion) {
+                    case "agregar":
+                        sql = "INSERT INTO amigos (nombre, direccion, telefono, email, dui, urlFoto) VALUES ('" + datos[1] + "', '" + datos[2] + "', '" + datos[3] + "', '" + datos[4] + "', '" + datos[5] + "', '" + datos[6] + "')";
+                        break;
+                    case "modificar":
+                        sql = "UPDATE amigos SET nombre = '" + datos[1] + "', direccion = '" + datos[2] + "', telefono = '" + datos[3] + "', email = '" + datos[4] + "', dui = '" + datos[5] + "', urlFoto = '" + datos[6] + "' WHERE idAmigo = " + datos[0];
+                        break;
+                    case "eliminar":
+                        sql = "DELETE FROM amigos WHERE idAmigo = " + datos[0];
+                        break;
+                }
+                // Ejecuta la sentencia SQL
+                db.execSQL(sql);
+                // Cierra la base de datos
+                db.close();
+                return mensaje;
+            } catch (Exception e) {
+                // Retorna el mensaje de error en caso de excepción
+                return e.getMessage();
             }
-            // Ejecuta la sentencia SQL
-            db.execSQL(sql);
-            // Cierra la base de datos
-            db.close();
-            return mensaje;
-        } catch (Exception e) {
-            // Retorna el mensaje de error en caso de excepción
-            return e.getMessage();
+        }
+
+        public Cursor lista_amigos (){
+            SQLiteDatabase db= getReadableDatabase();
+            return db.rawQuery("SELECT * FROM amigos", null);
         }
     }
-}
+
+
+
